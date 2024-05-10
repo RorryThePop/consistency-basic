d49b55da0326f2dbfbfaacfe881d79594475c80a
Arakelove <y.arakelov@ips.su>
2024-05-10 05:50:28 -0300

    DEV-02 modal added

--- 10-maio/src/App.jsx
+++ 10-maio/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./Component/Counter";
+import CustomButton from "./Component/UI/CustomButton";
+import Modal from "./Component/Modal/Modal";
 function App() {
+  const [isOpened, setIsOpened] = useState(false)
+
   return (
     <>
       <Counter />
+      <CustomButton text='click' onHandleClick={() => setIsOpened(!isOpened)}/>
+      {
+        isOpened && <Modal/>
+      }
     </>
   );
 }
---
+++ 10-maio/src/Component/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+import CustomButton from '../UI/CustomButton';
+import cls from './Modal.module.css'
+
+function Modal(props) {
+    return (
+        <div className={cls.modal}>
+            <div className={cls.modal__content}>
+            <h3>Modal</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus nisi officia temporibus consectetur labore unde soluta vitae cumque corporis porro doloremque suscipit neque, impedit consequuntur id ducimus, voluptatum dolores quis non? Beatae eaque corporis, quas pariatur dolore, incidunt, nam nulla praesentium vitae adipisci perferendis fuga saepe facere iste quod deserunt!</p>
+            <CustomButton text='close'/>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
