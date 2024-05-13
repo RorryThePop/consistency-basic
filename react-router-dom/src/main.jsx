a0eb2397f81c8f862dfe50f024c2587cb77980d3
Arakelove <y.arakelov@ips.su>
2024-05-13 07:19:54 -0300

    DEV-13-maio loader params

--- react-router-dom/src/main.jsx
+++ react-router-dom/src/main.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import Root from './routes/root';
-import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
-        element: <Contact/>
-      }
-    ]
-  }
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
